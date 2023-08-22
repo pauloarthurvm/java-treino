c6b86288e007f853181769a20783fa4fe492931b
Paulo Varassin <pauloarthurvm@hotmail.com>
2023-08-21 21:35:14 -0300

    Metodos post, put e delete

--- src/main/java/br/com/coruja/application/controller/HelloController.java
+++ src/main/java/br/com/coruja/application/controller/HelloController.java
@@ -28,7 +28,7 @@ public class HelloController {
     //  find deve buscar por um aluno específico recebendo o ID no path param - GET
 //  retornar 200 como status code
 //  caso não exista um aluno o valor retornado deve ser 404
-    @RequestMapping("/aluno")
+    @GetMapping("/aluno")
     public ResponseEntity<Aluno> getAluno(@RequestParam String id) {
         try {
             Long longId = Long.parseLong(id);
@@ -47,19 +47,39 @@ public class HelloController {
     //  list deve listar todos os alunos - GET
 //  retornar 200 como status code
 //  deve retornar uma coleção vazia com o status code 200
-    @RequestMapping("/alunos")
+    @GetMapping("/alunos")
     public ResponseEntity<List<Aluno>> getAlunos() {
         List<Aluno> aluno = alunoRepository.findAll();
         return new ResponseEntity<>(aluno, HttpStatus.OK);
     }
 
-//  save deve salvar um novo aluno - POST
+    //  save deve salvar um novo aluno - POST
 //  retornar 201 como status code
+    @PostMapping("/novoAluno")
+    public ResponseEntity<Aluno> novoAluno(@RequestBody Aluno novoAluno) {
+        Aluno aluno = alunoRepository.save(novoAluno);
+        return new ResponseEntity<>(aluno, HttpStatus.CREATED);
+    }
 
-//  put deve atualizar todos os atributos de um aluno recebendo o ID no path param - PUT
+    //  put deve atualizar todos os atributos de um aluno recebendo o ID no path param - PUT
 //  retornar 200 como status code
+    @PutMapping("/atualizarAluno/{id}")
+    public ResponseEntity<Aluno> atualizarAluno(@PathVariable String id, @RequestBody Aluno alunoAtualizado) {
+        Optional<Aluno> aluno = alunoRepository.findById(Long.parseLong(id));
+        if (aluno.isPresent()) {
+            aluno.get().setNome(alunoAtualizado.getNome());
+            aluno.get().setEmail(alunoAtualizado.getEmail());
+            return new ResponseEntity<>(alunoRepository.save(aluno.get()), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
-//  delete deve remover um aluno - DELETE
+    //  delete deve remover um aluno - DELETE
 //  retornar 200 como status code
+    @DeleteMapping("/deletarAluno/{id}")
+    public ResponseEntity<Aluno> deletarAluno(@PathVariable Long id) {
+        alunoRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
